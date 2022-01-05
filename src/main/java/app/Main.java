d30df5e7d7fda4f5978a0eb735d4bc7317638025
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2022-01-05 18:00:44 +0700

    Add runnable for performing background process

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -12,6 +12,8 @@ public class Main {
         URL storeURL = null;
         URL remoteURL = null;
         HTTPClientImplementation client = new HTTPClientImplementation();
+        Runnable runnable;
+        Thread thread;
 
         try {
             storeURL = new URL("file:///Users/alwi/Nazar/caches");
@@ -23,6 +25,21 @@ public class Main {
         LocalRestaurantLoader localLoader = new LocalRestaurantLoader(storeURL);
         RemoteRestaurantLoader remoteLoader = new RemoteRestaurantLoader(remoteURL, client);
         RemoteRestaurantImageDownloader imageDownloader = new RemoteRestaurantImageDownloader(remoteURL, client);
+
+        runnable = () -> {
+            System.out.println("Downloading...");
+            imageDownloader.load();
+
+            try {
+                Thread.sleep(10000);
+                System.out.println("Downloaded");
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        };
+
+        thread = new Thread(runnable);
+        thread.start();
 
         remoteLoader.load();
         localLoader.load();
