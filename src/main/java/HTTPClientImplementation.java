b4f5254a51779efedeb00a5815dfad48bd56b053
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2022-01-05 15:31:30 +0700

    Add `HTTPClientImplementation`

---
+++ src/main/java/HTTPClientImplementation.java
@@ -0,0 +1,10 @@
+import feature.HTTPClient;
+
+import java.net.URL;
+
+public class HTTPClientImplementation implements HTTPClient {
+    @Override
+    public void get(URL url) {
+        System.out.println("Get json data");
+    }
+}
