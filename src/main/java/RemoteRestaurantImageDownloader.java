d657edd329a81804a87f99cf6aef928e93f3d533
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2022-01-05 17:26:34 +0700

    Add `RemoteRestaurantImageDownloader`

---
+++ src/main/java/RemoteRestaurantImageDownloader.java
@@ -0,0 +1,18 @@
+import feature.HTTPClient;
+
+import java.net.URL;
+
+public class RemoteRestaurantImageDownloader {
+    URL url;
+    HTTPClient client;
+
+    public RemoteRestaurantImageDownloader(URL url, HTTPClient client) {
+        this.url = url;
+        this.client = client;
+    }
+
+    public void load(URL url) {
+        client.get(url);
+        System.out.println("Download restaurant image from database");
+    }
+}
